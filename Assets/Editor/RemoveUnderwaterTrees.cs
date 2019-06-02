063a7e615109c2553ccf13a7c13e20e45b1f809b
Kevin Dressel <kddressel@gmail.com>
2019-06-02 19:05:00 -0400

    new terrain textures and trees hooked up

---
+++ Assets/Editor/RemoveUnderwaterTrees.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public static class RemoveUnderwaterTrees
+{
+	[MenuItem("SWTG/Remove Underwater Trees")]
+	public static void Execute() 
+	{
+		var terrain = GameObject.FindObjectOfType<Terrain>();
+		Debug.Log("Removing trees below water level on " + terrain.name);
+
+		// get the width and depth of the terrain
+		var terrainSize = terrain.terrainData.size;
+		//Debug.Log( "terrainSize : " + terrainSize );
+
+		// get the tree data from the terrain data
+		var treeInstances = terrain.terrainData.treeInstances;
+		Debug.Log("Old : Total Trees = " + treeInstances.Length);
+
+		// create a list to store the modified information
+		var newTreeInstances = new List<TreeInstance>();
+
+		// calculate the normalized Water Level
+		var normalizedWaterLevel = -0.15f;
+
+		// cycle through each tree
+		for (var t = 0; t < treeInstances.Length; t++ )
+             {
+			// check if the tree Y is lower than the water level
+			if (treeInstances[t].position.y > normalizedWaterLevel) {
+				// if not, add tree to newTreeInstances List
+				newTreeInstances.Add(treeInstances[t]);
+			}
+		}
+
+		// apply newTreeInstances List to terrain data
+		terrain.terrainData.treeInstances = new TreeInstance[newTreeInstances.Count];
+		terrain.terrainData.treeInstances = newTreeInstances.ToArray();
+		Debug.Log("New : Total Trees = " + terrain.terrainData.treeInstances.Length);
+	}
+}
