ec8f1e8a31cf23ba163e6620c2fa67a748c5b4da
Donovan Fain <donovanpanian@gmail.com>
2020-03-21 14:07:33 -0400

    I made some "clouds" and they now randomly spawn in the mg area

--- Assets/Scripts/MiniGames/Storms/RandomizerForStorms.cs
+++ Assets/Scripts/MiniGames/Storms/RandomizerForStorms.cs
@@ -6,23 +6,26 @@ public class RandomizerForStorms : MonoBehaviour
 {
 	public GameObject miniGameWater;
 
-	private Vector3 randomSpawner;
 	private Vector3 randomMGwaterSize;
 
 	public GameObject miniGameRock;
-	public GameObject stormCloud; 
+	public GameObject[] stormClouds;
 
 void Start() {
 		//creates a randomly sized rectangle of water 
-		//better shape needed in the future
-		//maybe even an array of random shapes? 
 		randomMGwaterSize = new Vector3(Random.Range(20, 60), 1, Random.Range(20, 60));
 		miniGameWater.transform.localScale = randomMGwaterSize;
 
-		int rndNum = Random.Range(50, 100); 
+		//far fewer rocks will need to be included than clouds at this current time (03/21/2020)
+		int rndNumForRocks = Random.Range(50, 100);
+		int rndNumForClouds = Random.Range(400, 500);
 
-		for (int x = 0; x <= rndNum; x++) {
-			PopulateNearbyArea();
+		for (int x = 0; x <= rndNumForRocks; x++) {
+			PopulateNearbyAreaWithRocks();
+		}
+
+		for (int x = 0; x <= rndNumForClouds; x++) {
+			PopulateNearbyAreaWithClouds();
 		}
 	}
 
@@ -30,7 +33,7 @@ void Start() {
 		
 	}
 
-	private void PopulateNearbyArea() {
+	private void PopulateNearbyAreaWithRocks() {
 		//rocks need to spawn within the water's area
 		//spawning on x axis should occur between ((water.scale.x * 5)+ 1024) and ((water.scale.x * -5)+ 1024)  
 		//spawning on x axis should occur between ((water.scale.z * -5)- 948) and ((water.scale.z * 5)- 948)  
@@ -48,6 +51,33 @@ void Start() {
 		//ship start pnt = (1024, 782, -950)
 		if ((randomAreaForSpawningRocks.x > 1040 || randomAreaForSpawningRocks.x < 1015) && (randomAreaForSpawningRocks.z > -935 || randomAreaForSpawningRocks.z < -969)) {
 			Instantiate(miniGameRock, randomAreaForSpawningRocks, transform.rotation);
+		}
+	}
+
+	//the below method works very similarly to the above
+	//minor changes for spawning clouds have been implimented 
+	private void PopulateNearbyAreaWithClouds() {
+		//Clouds need to spawn within the water's area
+		//spawning on x axis should occur between ((water.scale.x * 5)+ 1024) and ((water.scale.x * -5)+ 1024)  
+		//spawning on x axis should occur between ((water.scale.z * -5)- 948) and ((water.scale.z * 5)- 948)  
+
+		float minX = ((miniGameWater.transform.localScale.x * -5) + 1024);
+		float maxX = ((miniGameWater.transform.localScale.x * 5) + 1024);
+
+		float minZ = ((miniGameWater.transform.localScale.z * -5) - 948);
+		float maxZ = ((miniGameWater.transform.localScale.z * 5) - 948);
+
+		//clouds will spawn in a similar matter, as they should only be within the minigame area
+		//the Y value will have an additional 50 units added to it, so that clouds will act more so like they should 
+		//Vector3 randomAreaForSpawningClouds = new Vector3(Random.Range(822, 1222), 808, Random.Range(-1150, -750));
+		Vector3 randomAreaForSpawningClouds = new Vector3(Random.Range(minX, maxX), 808, Random.Range(minZ, maxZ));
+
+		//clouds will follow the Rock spawn requirements as to make starting the MG more user friendly
+		//cannot have clouds spawn too close to the ship start point 
+		//ship start pnt = (1024, 782, -950)
+		if ((randomAreaForSpawningClouds.x > 1040 || randomAreaForSpawningClouds.x < 1015) && (randomAreaForSpawningClouds.z > -935 || randomAreaForSpawningClouds.z < -969)) {
+			int randCloud = Random.Range(0, 4);
+			Instantiate(stormClouds[randCloud], randomAreaForSpawningClouds, transform.rotation);
 		}
 	}
 
