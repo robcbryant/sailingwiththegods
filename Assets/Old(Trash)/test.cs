a27bc2eb54281fe02e9ef1a7051ed86fee8d1059
Kevin Dressel <kddressel@gmail.com>
2020-09-03 01:03:56 -0400

    upgrade to 2019.4.9f1

--- Assets/Old(Trash)/test.cs
+++ Assets/Old(Trash)/test.cs
@@ -11,18 +11,18 @@ public class test : MonoBehaviour {
 			TerrainData terrainData = terrain.terrainData;
 			
 			// Populate an array with current height data
-			float[,] orgHeightData = terrainData.GetHeights(0,0,terrainData.heightmapWidth, terrainData.heightmapHeight);
+			float[,] orgHeightData = terrainData.GetHeights(0,0,terrainData.heightmapResolution, terrainData.heightmapResolution);
 			
 			// Initialise a new array of same size to hold rotated data
-			float[,] mirroredHeightData = new float[terrainData.heightmapWidth, terrainData.heightmapHeight];
+			float[,] mirroredHeightData = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
 			
-			for (int y = 0; y < terrainData.heightmapHeight; y++)
+			for (int y = 0; y < terrainData.heightmapResolution; y++)
 			{
-				for (int x = 0; x < terrainData.heightmapWidth; x++)
+				for (int x = 0; x < terrainData.heightmapResolution; x++)
 				{
 					
 					// Rotate each element clockwise
-					mirroredHeightData[y,x] = orgHeightData[terrainData.heightmapHeight - y - 1, x];
+					mirroredHeightData[y,x] = orgHeightData[terrainData.heightmapResolution - y - 1, x];
 				}
 			}
 			
