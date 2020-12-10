1e2dca081a57e1b3e7b9f04ca83fec84beeef062
Kylie <kr.gilde@gmail.com>
2020-12-10 18:47:31 -0500

    Ship wake (storm only)
    Added particle systems to mimic a ship's wake to the storm minigame, with plans to add to the main game later

--- Assets/Scripts/MiniGames/Storms/StormMGmovement.cs
+++ Assets/Scripts/MiniGames/Storms/StormMGmovement.cs
@@ -10,6 +10,11 @@ public class StormMGmovement : MonoBehaviour
 	public float sizeMod = 0.16f;
 
 	private bool move;
+	private ParticleSystem[] ps;
+
+	private void Start() {
+		ps = GetComponentsInChildren<ParticleSystem>();
+	}
 
 	void Update() 
 	{
@@ -27,8 +32,25 @@ public class StormMGmovement : MonoBehaviour
 	{
 		float movement = Input.GetAxis("Vertical");
 		float rotation = Input.GetAxis("Horizontal");
+
 		transform.position += transform.right.normalized * sizeMod * movement * (movement > 0 ? speed : reverseSpeed);
 		transform.localEulerAngles += Vector3.up * rotSpeed * rotation;
+
+		if (Mathf.Approximately(0.0f, movement)) {
+			foreach (ParticleSystem p in ps) {
+				if (p.isPlaying) {
+					p.Stop();
+				}
+			}
+		}
+		else {
+			foreach (ParticleSystem p in ps) {
+				if (p.isStopped) {
+					p.Play();
+				}
+			}
+		}
+
 	}
 
 	public void ToggleMovement(bool toggle) 
